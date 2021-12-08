a02f116bb9ce506cb2ed7020c40f844dc9f71330
dantereto <2247@holbertonschool.com>
2021-12-08 11:14:38 -0500

    lock

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -2,17 +2,13 @@
 
 
 def canUnlockAll(boxes):
-    if not boxes:
-        return False
-    if len(boxes) == 0:
-        return False
-    keys = []
-    keys.append(0)
-    for key in keys:
-        new_keys = boxes[key]
-        for new_key in new_keys:
-            if new_key not in keys and new_key < len(boxes):
-                keys.append(new_key)
-    if len(keys) == len(boxes):
-        return True
-    return False
+    
+    for key in range(1, len(boxes) - 1):
+        ctr = False
+        for idx in range(len(boxes)):
+            ctr = (key in boxes[idx] and key != idx)
+            if ctr:
+                break
+        if ctr is False:
+            return ctr
+    return True
