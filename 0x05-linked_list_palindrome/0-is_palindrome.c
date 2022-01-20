5f93486731443f9ac789e81e0165184b09f80043
dantereto <2247@holbertonschool.com>
2022-01-20 23:44:11 +0000

    palindrome

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -9,21 +9,21 @@
  */
 int is_palindrome(listint_t **head)
 {
-    size_t i, siz;
-    int array[1000];
-    listint_t *temp;
-    temp = *head;
-    if (temp == NULL)
-        return (1);
-    else
-    {
-        for (siz = 0; temp != NULL; temp = temp->next, siz++)
-            array[siz] = temp->n;
-    }
-    for(i = 0; i < siz / 2; i++)
-    {
-        if (array[i] != array[siz -i -1])
-            return (0);
-    }
-    return (1);
+size_t i, siz;
+int array[1000];
+listint_t *temp;
+temp = *head;
+if (temp == NULL)
+return (1);
+else
+{
+for (siz = 0; temp != NULL; temp = temp->next, siz++)
+array[siz] = temp->n;
+}
+for (i = 0; i < siz / 2; i++)
+{
+if (array[i] != array[siz - i - 1])
+return (0);
+}
+return (1);
 }
