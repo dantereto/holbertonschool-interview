4eef17b848333aac39d4e1ced98b293c66b95f59
dantereto <2247@holbertonschool.com>
2022-01-27 22:29:32 -0500

    log parsing

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -5,7 +5,7 @@ import sys
 
 i = 0
 size = 0
-status = {'200': 0, '301': 0, '400': 0, '401': 0,
+STATUS = {'200': 0, '301': 0, '400': 0, '401': 0,
           '403': 0, '404': 0, '405': 0, '500': 0}
 codes = ['200', '301', '400', '401', 
          '403', '404', '405', '500']
@@ -15,17 +15,17 @@ try:
         space = line.split(' ')
         if len(space) > 2:
             size += int(space[-1])
-            if space[-2] in status:
-                status[space[-2]] += 1
+            if space[-2] in STATUS:
+                STATUS[space[-2]] += 1
         if i % 10 == 0:
             print("File size: {}".format(size))
             for code in codes:
-                if status[code]:
-                    print("{}: {}".format(code, status[code]))
+                if STATUS[code]:
+                    print("{}: {}".format(code, STATUS[code]))
 except KeyboardInterrupt:
     pass
 finally:
     print("File size: {}".format(size))
     for code in codes:
-        if status[code]:
-            print("{}: {}".format(code, status[code]))
+        if STATUS[code]:
+            print("{}: {}".format(code, STATUS[code]))
