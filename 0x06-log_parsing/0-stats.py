9ef4da442a2970055f2ce3ff14da0baa7e88d813
dantereto <2247@holbertonschool.com>
2022-01-30 11:33:36 -0500

    corrct

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,30 +1,30 @@
 #!/usr/bin/python3
-""" create the function"""
+""" create the funciton """
 import sys
 
-
 i = 0
-FileSize = 0
+size = 0
 status = {'200': 0, '301': 0, '400': 0, '401': 0,
           '403': 0, '404': 0, '405': 0, '500': 0}
-codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+codes = ['200', '301', '400', '401', 
+         '403', '404', '405', '500']
 try:
     for line in sys.stdin:
         i += 1
-        sp = line.split(' ')
-        if len(sp) > 2:
-            FileSize += int(sp[-1])
-            if sp[-2] in status:
-                status[sp[-2]] += 1
+        space = line.split(' ')
+        if len(space) > 2:
+            size += int(space[-1])
+            if space[-2] in status:
+                status[space[-2]] += 1
         if i % 10 == 0:
-            print("File size: {}".format(FileSize))
+            print("File size: {}".format(size))
             for code in codes:
                 if status[code]:
                     print("{}: {}".format(code, status[code]))
 except KeyboardInterrupt:
     pass
 finally:
-    print("File size: {}".format(FileSize))
+    print("File size: {}".format(size))
     for code in codes:
         if status[code]:
             print("{}: {}".format(code, status[code]))
