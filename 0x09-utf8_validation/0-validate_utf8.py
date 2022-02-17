5aae48dbbddf5b28258b060b68476190045cceb4
dantereto <2247@holbertonschool.com>
2022-02-17 10:28:49 -0500

    utf-8

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Valid utf
+"""
+
+
+def validUTF8(data):
+
+    number = 0
+    for n in data:
+        byte = format(n, '#010b')[-8:]
+        if number == 0:
+            if number > 4 or number == 1:
+                return False
+            if byte[0] == '1':
+                number = len(byte.split('0')[0])
+            if number == 0:
+                continue
+        else:
+            if not byte.startswith('10'):
+                return False
+        number -= 1
+    if number != 0:
+        return False
+    return True
