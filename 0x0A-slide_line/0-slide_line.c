808927700054a864dfdbef6523986d54b3e1ec4c
dantereto <2247@holbertonschool.com>
2022-02-24 20:20:56 -0500

    slide

---
+++ 0x0A-slide_line/0-slide_line.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "slide_line.h"
+
+/**
+ * is_palindrome - check if the linked list is a palindrome
+ * @head: head of list
+ * Return: 0
+ */
+int slide_line(int *line, size_t size, int direction)
+{
+    int cont = -1;
+    size_t posf = 0, posr = 0, i = 1, copy_s = size;
+
+	if (direction != SLIDE_LEFT && direction != SLIDE_RIGHT)
+		return (0);
+	if (direction == SLIDE_RIGHT)
+		posf = size - 1, posr = size - 1, i = -1, copy_s = -1;
+	while (posf != copy_s)
+	{
+		if (*(line + posf) != 0)
+		{
+			if (cont == *(line + posf))
+				*(line + posf) += cont, cont = -1;
+			else
+			{
+				cont = *(line + posf);
+				if ((direction == SLIDE_LEFT && posf != 0) ||
+				   ((direction == SLIDE_RIGHT && posf != (size - 1))))
+					if (*(line + posr) != 0)
+						posr += i;
+			}
+			*(line + posr) = *(line + posf);
+			if (posr != posf)
+				*(line + posf) = 0;
+		}
+		posf += i;
+	}
+	return (1);
+}
