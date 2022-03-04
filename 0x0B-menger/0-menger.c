d3c9c42d8974254dfc7f2de0da35510f49bed508
dantereto <2247@holbertonschool.com>
2022-03-03 22:55:19 -0500

    menger

--- 0x0B-menger/0-menger.c
+++ 0x0B-menger/0-menger.c
@@ -6,23 +6,23 @@
 **/
 void menger(int level)
 {
-int size = pow(3, level);
-int i, j, k, m;
-char square;
-for (i = 0; i < size; i++)
-{
-for (j = 0; j < size; j++)
-{
-square = '#';
-k = i;
-for (m = j; m; m /= 3)
-{
-if (k % 3 == 1 && m % 3 == 1)
-square = ' ';
-k = k / 3;
-}
-printf("%c", square);
-}
-printf("\n");
-}
+    int size = pow(3, level);
+    int i, j, k, m;
+    char square;
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < size; j++)
+        {
+            square = '#';
+            k = i;
+            for (m = j; m; m /= 3)
+            {
+                if (k % 3 == 1 && m % 3 == 1)
+                square = ' ';
+                k = k / 3;
+            }
+            printf("%c", square);
+        }
+    printf("\n");
+    }
 }
