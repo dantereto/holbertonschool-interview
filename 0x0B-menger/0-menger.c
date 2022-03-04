50279d88f796252750d1cf06290bdf575c9a848a
dantereto <2247@holbertonschool.com>
2022-03-03 22:31:40 -0500

    menger

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,28 @@
+#include "menger.h"
+/**
+* menger - print menger
+* @level: level
+* Return: 0
+**/
+void menger(int level)
+{
+int size = pow(3, level);
+int i, j, k, m;
+char square;
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+square = '#';
+k = i;
+for (m = j; m; m /= 3)
+{
+if (k % 3 == 1 && m % 3 == 1)
+square = ' ';
+k = k / 3;
+}
+printf("%c", square);
+}
+printf("\n");
+}
+}
