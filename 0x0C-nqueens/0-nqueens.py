4b1882c586ca91e854d8788741fa577e8694f110
dantereto <2247@holbertonschool.com>
2022-03-17 23:33:12 +0000

    queens

---
+++ 0x0C-nqueens/0-nqueens.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" create the funciton """
+import sys
+
+def argv_parse():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    if not sys.argv[1].isdigit():
+        print("N must be a number")
+        sys.exit(1)
+    n = int(sys.argv[1])
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    return n
+    
+def nqueens(queens, diff, sum):
+    p = len(queens)
+    if p == n:
+        queen_n.append(queens)
+        return None
+    for q in range(n):
+        if q not in queens and p - q not in diff and p + q not in sum:
+            nqueens(queens + [q], diff + [p - q], sum + [p + q])
+
+
+if __name__ == "__main__":
+    n = argv_parse()
+    queen_n = []
+    nqueens([], [], [])
+    for row in range(len(queen_n)):
+        queen_p = []
+        for col in range(len(queen_n[row])):
+            queen_p.append([col, queen_n[row][col]])
+        print(queen_p)
