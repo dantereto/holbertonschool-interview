2460711d18d2c28ef6130b30d27bb76320dc2bbe
dantereto <2247@holbertonschool.com>
2022-03-30 22:24:31 -0500

    linear skip

--- 0x0E-linear_skip/0-linear_skip.c
+++ 0x0E-linear_skip/0-linear_skip.c
@@ -3,42 +3,42 @@
 #include "search.h"
 
 /**
-* linear_skip - Linear skip
-* @list: list to check
-* @value: the value to found
-*
-* Return: 0
-*/
-skiplist_t *linear_skip(skiplist_t *list, int value)
+ * linear_skip - Linear skip
+ * @list: list to check
+ * @value: the value to found
+ *
+ * Return: 0
+ */
+ skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-skiplist_t *copy;
-skiplist_t *p;
-if (list == NULL)
-return (NULL);
-copy = list->express;
-p = list;
-while (copy != NULL)
-{
-printf("Value checked at index [%lu] = [%d]\n", copy->index, copy->n);
-if (copy->n >= value)
-break;
-p = copy;
-if (copy->express == NULL)
-{
-while (copy->next)
-copy = copy->next;
-break;
-}
-copy = copy->express;
-}
-printf("Value found between indexes [%lu] and [%lu]\n",
-p->index, copy->index);
-while (p && p->index <= copy->index)
-{
-printf("Value checked at index [%lu] = [%d]\n", p->index, p->n);
-if (p->n == value)
-return (p);
-p = p->next;
-}
-return (NULL);
+    skiplist_t *copy;
+    skiplist_t *p;
+    if (list == NULL)
+        return (NULL);
+    copy = list->express;
+    p = list;
+    while (copy != NULL)
+    {
+        printf("Value checked [%lu] at index [%d]\n", copy->index, copy->n);
+        if (copy->n >= value)
+            break;
+        p = copy;
+        if (copy->express == NULL)
+        {
+            while (copy->next)
+                copy = copy->next;
+            break;
+        }
+        copy = copy->express;
+    }
+    printf("Value found between indexes [%lu] and [%lu]\n",
+    p->index, copy->index);
+    while (p && p->index <= copy->index)
+    {
+        printf("Value checked [%lu] at index [%d]\n", p->index, p->n);
+        if (p->n == value)
+            return (p);
+        p = p->next;
+    }
+    return (NULL);
 }
