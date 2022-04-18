2ec1c65ea2e1d21909cfe894300dfcf4b8ad89a4
dantereto <2247@holbertonschool.com>
2022-04-18 02:54:32 -0500

    heap

---
+++ 0x11-heap_sort/0-heap_sort.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "sort.h"
+
+/**
+ * linear_skip - Linear search in a skip list
+ *
+ * @list: Pointer to the head node of the list
+ * @value: value to check
+ * Return:0
+ */
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+  }
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+  
+    if (left < n && arr[left] > arr[largest])
+      largest = left;
+  
+    if (right < n && arr[right] > arr[largest])
+      largest = right;
+    if (largest != i) {
+      swap(&arr[i], &arr[largest]);
+      heapify(arr, n, largest);
+    }
+}
+void heap_sort(int *array, size_t size)
+{
+    for (int i = size / 2 - 1; i >= 0; i--)
+      heapify(arr, size, i);
+    for (int i = n - 1; i >= 0; i--) {
+      swap(&arr[0], &arr[i]);
+    heapify(arr, i, 0);
+}
---
+++ 0x11-heap_sort/sort.h
@@ -0,0 +1,11 @@
+#ifndef LIST_H_
+#define LIST_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void heap_sort(int *array, size_t size);
+print_array(const int *array, size_t size);
+
+#endif
