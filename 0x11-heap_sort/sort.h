2c250c9d0f7850dff5162ff5bada295b79f852af
dantereto <2247@holbertonschool.com>
2022-04-19 21:53:20 +0000

    heap

--- 0x11-heap_sort/sort.h
+++ 0x11-heap_sort/sort.h
@@ -7,7 +7,7 @@
 
 void heap_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
-void heapify(int arr[], int n, int i);
+void heapify(int *array, int n, int i, size_t size);
 void swap(int *a, int *b);
 
 #endif
