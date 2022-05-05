cf69d5ac0d5a59067eb8a9d1895df02d278af2bc
dantereto <2247@holbertonschool.com>
2022-05-05 18:02:50 -0500

    count

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -4,7 +4,7 @@ import requests
 
 
 def count_words(subreddit, word_list, dic={}, after=None):
-    """ count words from list in subreddit titles """
+    """Count"""
     if len(dic) == 0:
         for word in word_list:
             dic[word] = 0
@@ -16,14 +16,14 @@ def count_words(subreddit, word_list, dic={}, after=None):
             p['after'] = after
         r = requests.get(u, headers=h, params=p, allow_redirects=False).json()
         posts = r.get('data').get('children')
-        for x in posts:
+        for i in posts:
             for word in word_list:
-                if word.lower() in x.get('data').get('title').lower().split():
+                if word.lower() in i.get('data').get('title').lower().split():
                     dic[word] += 1
         after = r.get('data').get('after')
         if after:
             return count_words(subreddit, word_list, dic, after)
-        for k, v in sorted(dic.items(), key=lambda x: x[1], reverse=True):
+        for k, v in sorted(dic.items(), key=lambda i: i[1], reverse=True):
             if v > 0:
                 print('{}: {}'.format(k, v))
 
