a0a660607f193b9248ba5c9946615456b8de9ec1
dantereto <2247@holbertonschool.com>
2022-06-23 15:16:29 -0500

    correct

---
+++ 0x19-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""create the function"""
+
+def makeChange(coins, total):
+    if total <= 0:
+        return 0
+    coins.sort(reverse=True)
+    copy = total
+    mv = 0
+    cont = 0
+
+    while (mv < len(coins)):
+        if copy == 0:
+            return cont
+
+        if coins[mv] > copy:
+            mv += 1
+
+        else:
+            copy -= coins[mv]
+            cont += 1
+    return -1
